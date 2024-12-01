84b8b203c953c7be5fb757750a4bb0e25cce2b16
Elena Korchuganova <korchel21@gmail.com>
2024-12-01 19:43:46 +0300

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import { Container, Title } from "@/components/shared";
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <>
+      <Container>
+        <Title text="All pizzas" size="lg" />
+      </Container>
+    </>
+  );
+}
---
+++ components/shared/Header.tsx
@@ -0,0 +1,43 @@
+import { cn } from "@/lib/utils";
+import React, { FC } from "react";
+import { Container } from "./Container";
+import Image from "next/image";
+import { Button } from "../ui";
+import { ArrowRight, ShoppingCart, User } from "lucide-react";
+
+interface IHeaderProps {
+  className?: string;
+}
+
+export const Header: FC<IHeaderProps> = ({ className }) => {
+  return (
+    <header className={cn('border border-b', className) }>
+      <Container className="flex items-center justify-between py-8">
+        <div className="flex items-center gap-4">
+          <Image src="/logo.png" alt="logo" width={35} height={35} />
+          <div>
+            <h1 className="text-2xl uppercase font-black">Next Pizza</h1>
+            <p className="text-sm text-gray-400 leading-3">Вкуснее уже некуда</p>
+          </div>
+        </div>
+        <div className="flex items-center gap-3">
+          <Button className="flex items-center gap-1" variant='outline'>
+            <User size={16} />
+            Выйти
+          </Button>
+          <div>
+            <Button className="group relative">
+              <b>520 p</b>
+              <span className="h-full w-[1px] bg-white/30 mx-3" />
+              <div className="flex items-center gap-1 transition duration-300 group-hover:opacity-0">
+                <ShoppingCart size={16} strokeWidth={2} />
+                <b>3</b>
+              </div>
+              <ArrowRight size={20} className="absolute right-5 transition duration-300 -translate-x-2 opacity-0 group-hover:opacity-100 group-hover:translate-x-0" />
+            </Button>
+          </div>
+        </div>
+      </Container>
+    </header>
+  );
+};
