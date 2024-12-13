3e2db0725fa576fe08299c2bfce3df99e8e76c45
korchel <korchel21@gmail.com>
2024-12-13 14:01:27 +0300

    filters state edited

--- components/shared/Filters.tsx
+++ components/shared/Filters.tsx
@@ -1,30 +1,89 @@
-'use client';
+"use client";
 
 import { cn } from "@/lib/utils";
-import { FC } from "react";
+import { FC, useEffect, useState } from "react";
+import qs from "qs";
 import { Title } from "./Title";
-import { FilterCheckbox } from "./FilterCheckbox";
 import { Input } from "../ui";
 import { RangeSlider } from "./RangeSlider";
 import { CheckboxFiltersGroup } from "./CheckboxFiltersGroup";
 import { useFilterIngredients } from "@/hooks/useFilterIngredients";
+import { useSet } from "react-use";
+import { useRouter, useSearchParams } from "next/navigation";
 
 interface IFiltersProps {
   className?: string;
 }
 
-export const Filters: FC<IFiltersProps> = ({ className }) => {
-  const { ingredients, loading, onAddId, selectedIds } = useFilterIngredients();
+interface IPriceRange {
+  from?: number;
+  to?: number;
+}
 
-  const items = ingredients.map((ingredient) => ({ value: String(ingredient.id), text: ingredient.name }));
+export const Filters: FC<IFiltersProps> = ({ className }) => {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+
+  console.log(searchParams);
+
+  const [pizzaTypes, { toggle: togglePizzaType }] = useSet<string>(new Set([]));
+  const [sizes, { toggle: toggleSizes }] = useSet<string>(new Set([]));
+  const { ingredients, loading, onAddId, selectedIngredientIds } =
+    useFilterIngredients();
+  const [priceRange, setPriceRange] = useState<IPriceRange>({});
+
+  const items = ingredients.map((ingredient) => ({
+    value: String(ingredient.id),
+    text: ingredient.name,
+  }));
+
+  const changePriceRange = (name: keyof IPriceRange, value: number) => {
+    setPriceRange({
+      ...priceRange,
+      [name]: value,
+    });
+  };
+
+  useEffect(() => {
+    const currentFilters = {
+      ...priceRange,
+      pizzaTypes: Array.from(pizzaTypes),
+      sizes: Array.from(sizes),
+      ingredients: Array.from(selectedIngredientIds),
+    };
+    const query = qs.stringify(currentFilters, {
+      arrayFormat: "comma",
+      skipNulls: true,
+    });
+    router.push(`/?${query}`, { scroll: false });
+  }, [priceRange, pizzaTypes, selectedIngredientIds, sizes]);
 
   return (
     <div className={cn(className)}>
       <Title text="filter" size="sm" className="mb-5 font-bold" />
-      <div className="flex flex-col gap-4">
-        <FilterCheckbox text="can combine?" value="1" />
-        <FilterCheckbox text="new" value="2" />
-      </div>
+      <CheckboxFiltersGroup
+        name="pizzaTypes"
+        title="Pizza types"
+        className="mb-5"
+        selected={pizzaTypes}
+        items={[
+          { text: "Thin", value: "1" },
+          { text: "Traditional", value: "2" },
+        ]}
+        onCheck={togglePizzaType}
+      />
+      <CheckboxFiltersGroup
+        name="sizes"
+        title="Sizes"
+        className="mb-5"
+        selected={sizes}
+        items={[
+          { text: "20cm", value: "20" },
+          { text: "30cm", value: "30" },
+          { text: "40cm", value: "40" },
+        ]}
+        onCheck={toggleSizes}
+      />
       <div className="mt-5 border-y border-y-neutral-100 py-6 pb-7">
         <p>Price from to</p>
         <div className="flex gap-3 mb-5">
@@ -34,12 +93,28 @@ export const Filters: FC<IFiltersProps> = ({ className }) => {
             min={0}
             max={1000}
             defaultValue={0}
+            value={priceRange.from}
+            onChange={(e) => changePriceRange("from", +e.target.value)}
           />
-          <Input type="number" placeholder="30000" min={100} max={30000} />
+          <Input
+            type="number"
+            placeholder="1000"
+            min={100}
+            max={1000}
+            value={priceRange.to}
+            onChange={(e) => changePriceRange("to", +e.target.value)}
+          />
         </div>
-        <RangeSlider min={0} max={5000} step={10} value={[0, 5000]} />
+        <RangeSlider
+          min={0}
+          max={1000}
+          step={10}
+          value={[priceRange.from || 0, priceRange.to || 1000]}
+          onValueChange={([from, to]) => setPriceRange({ from, to })}
+        />
       </div>
       <CheckboxFiltersGroup
+        name="ingredients"
         title="ingeridents"
         className="mt-5"
         limit={6}
@@ -47,7 +122,7 @@ export const Filters: FC<IFiltersProps> = ({ className }) => {
         items={items}
         loading={loading}
         onCheck={onAddId}
-        selectedIds={selectedIds}
+        selected={selectedIngredientIds}
       />
     </div>
   );
--- hooks/useFilterIngredients.ts
+++ hooks/useFilterIngredients.ts
@@ -1,19 +1,19 @@
 import { api } from "@/services/apiClient";
-import { Ingredient } from "@prisma/client"
+import { Ingredient } from "@prisma/client";
 import { useEffect, useState } from "react";
 import { useSet } from "react-use";
 
 interface IFilteredIngredients {
   ingredients: Ingredient[];
   loading: boolean;
-  selectedIds: Set<string>;
+  selectedIngredientIds: Set<string>;
   onAddId: (id: string) => void;
 }
 
 export const useFilterIngredients = (): IFilteredIngredients => {
   const [ingredients, setIngredients] = useState<Ingredient[]>([]);
   const [loading, setLoading] = useState(true);
-  const [selectedIds, { toggle }] = useSet<string>(new Set([]));
+  const [selectedIngredientIds, { toggle }] = useSet<string>(new Set([]));
 
   useEffect(() => {
     setLoading(true);
@@ -24,9 +24,9 @@ export const useFilterIngredients = (): IFilteredIngredients => {
       })
       .catch((error) => console.error(error))
       .finally(() => {
-        setLoading(false)
+        setLoading(false);
       });
   }, []);
 
-  return { ingredients, loading, selectedIds, onAddId: toggle };
+  return { ingredients, loading, selectedIngredientIds, onAddId: toggle };
 };
