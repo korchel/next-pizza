1253de6525fa49b3d100985d93671e5420be07b8
Elena Korchuganova <korchel21@gmail.com>
2024-12-17 21:56:19 +0300

    pizza from edited

--- components/shared/modals/ProductModal.tsx
+++ components/shared/modals/ProductModal.tsx
@@ -1,14 +1,14 @@
 'use client';
 
 import { Dialog, DialogContent } from "@/components/ui";
-import { Product } from "@prisma/client";
+import { Ingredient, Product, ProductVariant } from "@prisma/client";
 import { FC } from "react";
 import { useRouter } from "next/navigation";
 import { ProductForm } from "../ProductForm";
 import { PizzaForm } from "../PizzaForm";
 
 interface IProductModalProps {
-  product: Product;
+  product: Product & { variants: ProductVariant[]; ingredients: Ingredient[] };
   className?: string;
 }
 
@@ -21,11 +21,16 @@ export const ProductModal: FC<IProductModalProps> = ({ product }) => {
     <Dialog open={Boolean(product)} onOpenChange={() => router.back()}>
       <DialogContent className="p-0 w-[1060px] max-w-[1060] min-h-[550px] bg-white overflow-hidden">
         {isPizza ? (
-          <PizzaForm imageUrl={product.imageUrl} name={product.name} ingredients={[]} onClickAdd={() => {}} />
+          <PizzaForm
+            imageUrl={product.imageUrl}
+            name={product.name}
+            ingredients={product.ingredients}
+            variants={product.variants}
+            addToCart={() => { }}
+          />
         ) : (
           <ProductForm imageUrl={product.imageUrl} name={product.name} addToCart={() => {}} />
         )}
-        
       </DialogContent>
       
     </Dialog>
