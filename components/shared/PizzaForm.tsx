ea5e10d6a5ee9cea01d4bae636574f66fd766275
Elena Korchuganova <korchel21@gmail.com>
2024-12-17 21:56:19 +0300

    pizza from edited

--- components/shared/PizzaForm.tsx
+++ components/shared/PizzaForm.tsx
@@ -1,16 +1,20 @@
 import { cn } from "@/shared/lib/utils";
 import { FC, useState } from "react";
+import { useSet } from "react-use";
+
 import { PizzaImage } from "./PizzaImage";
 import { Title } from "./Title";
 import { Button } from "../ui";
 import { PizzaTypeSelector } from "./PizzaTypeSelector";
 import { PizzaSize, pizzaSizes, PizzaType, pizzaTypes } from "@/shared/constants/pizza";
+import { Ingredient, ProductVariant } from "@prisma/client";
+import { IngredientTag } from "./IngredientTag";
 
 interface IPizzaFormProps {
   imageUrl: string;
   name: string;
-  ingredients: any[];
-  items?: any[];
+  ingredients: Ingredient[];
+  variants: ProductVariant[];
   addToCart: VoidFunction;
   className?: string;
 }
@@ -19,21 +23,27 @@ export const PizzaForm: FC<IPizzaFormProps> = ({
   imageUrl,
   name,
   ingredients,
-  items,
+  variants,
   addToCart,
   className
 }) => {
-  const [size, setSize] = useState<PizzaSize>(20);
-   const [type, setType] = useState<PizzaType>(1);
-  const details = '30 cm traditional';
+  const [selectedIngredients, { toggle: addIngredient }] = useSet(new Set<number>([]));
 
+  const [size, setSize] = useState<PizzaSize>(20);
+  const [type, setType] = useState<PizzaType>(1);
+
+  const pizzaPrice = variants.find((item) => item.pizzaType === type && item.size === size)?.price || 0;
+  const ingredientsPrice = ingredients
+    .filter((ingredient) => selectedIngredients.has(ingredient.id))
+    .reduce((acc, ingredient) => acc + ingredient.price, 0);
+  const totalPrice = pizzaPrice + ingredientsPrice;
 
   return (
     <div className={cn(className, 'flex flex-1')}>
       <PizzaImage imageUrl={imageUrl} size={size} />
       <div className="w-[490px] bg-[#f7f6f5] p-7">
         <Title text={name} size="md" className="font-extrabold mb-1" />
-        <p className="text-gray-400">{details}</p>
+        <p className="text-gray-400">30 cm traditional</p>
         <PizzaTypeSelector
           items={pizzaSizes}
           selectedValue={String(size)}
@@ -44,8 +54,23 @@ export const PizzaForm: FC<IPizzaFormProps> = ({
           selectedValue={String(type)}
           onClick={(value) => setType(Number(value) as PizzaType)}
         />
+        <div className="pg-gray-50 p-5 mt-5 rounded-md h-[420px] overflow-auto scrollbar">
+          <div className="grid grid-cols-3 gap-3">
+          {ingredients.map((ingredient) => (
+            <IngredientTag
+              key={ingredient.id}
+              name={ingredient.name}
+              price={ingredient.price}
+              imageUrl={ingredient.imageUrl}
+              onClick={() => addIngredient(ingredient.id)}
+              active={selectedIngredients.has(ingredient.id)}
+            />
+          ))}
+        </div>
+        </div>
+        
         <Button className="h-[55px] px-10 text-base rounded-[18px] w-full mt-10" onClick={addToCart}>
-          ffff
+          Add to cart for {totalPrice}
         </Button>
       </div>
     </div>
