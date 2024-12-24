a2c1564455d3272bcdb0c7f84e33a0694fc2b9cf
Elena Korchuganova <korchel21@gmail.com>
2024-12-24 22:10:58 +0300

    gart edited

--- components/shared/CartButton.tsx
+++ components/shared/CartButton.tsx
@@ -1,20 +1,23 @@
+'use client';
+
 import { cn } from "@/shared/lib/utils";
 import { FC } from "react";
 import { Button } from "../ui";
 import { ArrowRight, ShoppingCart } from "lucide-react";
 import { CartDrawer } from "./cartDrawer/CartDrawer";
+import { useCartStore } from "@/shared/store";
 
 interface Props {
   className?: string;
 }
 
 export const CartButton: FC<Props> = ({ className }) => {
-
+    const { totalCost, loading } = useCartStore();
 
   return (
     <CartDrawer>
-      <Button className={cn(className, "group relative")}>
-        <b>520 p</b>
+      <Button loading={loading} className={cn(className, "group relative")}>
+        <b>{totalCost} p</b>
         <span className="h-full w-[1px] bg-white/30 mx-3" />
         <div className="flex items-center gap-1 transition duration-300 group-hover:opacity-0">
           <ShoppingCart size={16} strokeWidth={2} />
