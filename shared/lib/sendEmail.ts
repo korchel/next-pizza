fd66847098b78a7a272cf4c51f5f065b145ca994
Elena Korchuganova <korchel21@gmail.com>
2025-02-02 12:59:06 +0300

    nodemailer edited

--- shared/lib/sendEmail.ts
+++ shared/lib/sendEmail.ts
@@ -29,12 +29,7 @@ import SMTPTransport from "nodemailer/lib/smtp-transport";
 
 const transport = nodemailer.createTransport({
   host: process.env.MAIL_HOST,
-  port: process.env.MAIL_PORT,
-  secure: process.env.NODE_ENV !== "development",
   auth: { user: process.env.MAIL_USER, pass: process.env.MAIL_PASSWORD },
-  tls: {
-    rejectUnauthorized: false,
-  },
 } as SMTPTransport.Options);
 
 interface SendEmail {
